2e216ebff9c21ba09fdc8963b5ad50b36929516e
Chen <89332989+ClorisXue93@users.noreply.github.com>
2021-10-01 10:47:26 -0700

    add function 1

---
+++ function.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+
+void testFunc(int test_twoDimArray[3][3]) {
+
+    //check first row
+    int sum = 0;
+    int check_LoShu = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[0][i];
+    }
+    if (sum == 15) {
+        printf("First line is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("First line is NOT Lo Shu\n");
+    //check second row
+    sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[1][i];
+    }
+    if (sum == 15) {
+        printf("Second line is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("Second line is NOT Lo Shu\n");
+
+    //check third row
+    sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[2][i];
+    }
+    if (sum == 15) {
+        printf("Third line is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("Third line is NOT Lo Shu\n");
+
+    //check first column
+    sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[i][0];
+    }
+    if (sum == 15) {
+        printf("First column is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("First column is NOT Lo Shu\n");
+
+    //check second column
+    sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[i][1];
+    }
+    if (sum == 15) {
+        printf("Second column is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("Second column is NOT Lo Shu\n");
+
+    //check third column
+    sum = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[i][2];
+    }
+    if (sum == 15) {
+        printf("First line is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("First line is NOT Lo Shu\n");
+
+    //check diagonal
+    sum = 0;
+    int j = 0;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[i][j];
+        j++;
+    }
+    if (sum == 15) {
+        printf("Diagonal left is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("Diagonal left is NOT Lo Shu\n");
+
+    sum = 0;
+    j = 2;
+    for (int i = 0; i < 3; i++) {
+        sum += test_twoDimArray[i][j];
+        j--;
+    }
+    if (sum == 15) {
+        printf("Diagonal right is Lo Shu\n");
+        check_LoShu += 1;
+    }
+    else 
+        printf("Diagonal right is NOT Lo Shu\n");
+
+    if (check_LoShu == 8) {
+        printf("This test array is Lo Shu Magic Square\n");
+    }
+    else
+        printf("This test array is NOT Lo Shu Magic Square\n");
+}
+
+void getArray(int twoDimArray[3][3]) {
+    int numberArray[9];
+}
--- main.c
+++ main.c
@@ -1,1 +1,12 @@
-#include <stdio.h>
+#include "function.c"
+
+void main (void) {
+    int twoDimArray[3][3] = {
+        {4, 9, 2},
+        {3, 5, 7},
+        {8, 1, 6}
+    };
+
+    testFunc(twoDimArray);
+    
+}
